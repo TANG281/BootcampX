3f13a105645c827ef8444e44464a7ea3e6e691dd
Tuan Anh Nguyen <atngu312@gmail.com>
2023-08-30 06:40:42 -0400

    parameterized query string

--- bootcamp_app/teacher.js
+++ bootcamp_app/teacher.js
@@ -9,16 +9,19 @@ const pool = new Pool({
 
 const cohortName = process.argv[2];
 
+const values = [`%${cohortName || 'JUL02'}%`];
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${cohortName || 'JUL02'}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teachers.name;
-`)
+`;
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach((teacher) => {
       console.log(`${teacher.cohort}: ${teacher.teacher}`);
